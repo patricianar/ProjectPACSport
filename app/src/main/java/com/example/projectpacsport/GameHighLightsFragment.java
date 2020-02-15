73743982bb356e7d69a27c068bec2a7a73a6c2e1
patricianar <57020002+patricianar@users.noreply.github.com>
2020-02-15 00:18:32 -0800

    crashing app solved

--- app/src/main/java/com/example/projectpacsport/GameHighLightsFragment.java
+++ app/src/main/java/com/example/projectpacsport/GameHighLightsFragment.java
@@ -9,11 +9,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
 public class GameHighLightsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_game_highlights, container, false);
+
+        YouTubePlayerView youTubePlayerView = view.findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "BCJ8p7pOTvE";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+
         return view;
     }
 }
--- app/src/main/java/com/example/projectpacsport/Player.java
+++ app/src/main/java/com/example/projectpacsport/Player.java
@@ -5,6 +5,12 @@ import java.io.Serializable;
 public class Player implements Serializable {
     private String firstName;
     private String lastName;
+    private String birthday;
+    private String birthPlace;
+    private String team;
+
+    private double height;
+    private double weight;
 
     public String getFirstName() {
         return firstName;
@@ -20,5 +26,45 @@ public class Player implements Serializable {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 }
