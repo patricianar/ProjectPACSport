d9413e9b81f0bce1faee13a09257a45d28d8e6b8
Chau Ng <ngocchau0710@hotmail.com>
2020-02-15 05:36:42 -0800

    Layout Player added - chau

---
+++ app/src/main/java/com/example/projectpacsport/player/PlayerActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectpacsport.player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectpacsport.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
